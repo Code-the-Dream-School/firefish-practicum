8acf958b66f85757fb496000ba184da81741e735
Ron Javellana <Ronapj1991@gmail.com>
2024-04-02 10:58:55 -0400

    testing devise helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,8 +30,9 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers
 
+  # Remove this line if you're not using ActiveRecord or ActiveRecord 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
