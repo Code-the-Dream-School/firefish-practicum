c4b0e22456deb4fdf76088c002c870579b7547a8
Ron Javellana <Ronapj1991@gmail.com>
2024-04-06 19:56:53 -0400

    test data extraction from api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,17 @@ class HomeController < ApplicationController
         #     req.params['text'] = "Venice Italy"
         #     req.params['format'] = "json"
         # end #response_for_geocode_api.status => 200
+        # @response_geo = JSON.parse(response_for_geocode_api.body)
+        # @city_place_id = @response_geo['results'][0]['place_id']
 
         # response_for_places_api = GeoapifyClient.connection.get("/v2/places") do |req|
         #     req.params["categories"] = "tourism.attraction"
-        #     req.params["filter"] = "place:517e26b15a4fab284059590e3edef5b74640f00101f901c5ae000000000000c00207"
-        #     req.params["limit"] = 5 #using 5 for now while testing
+        #     req.params["filter"] = "place:" + @city_place_id
+        #     req.params["limit"] = 2 #using 5 for now while testing
         # end #response_for_places_api.status => 200
+        # @response_place = JSON.parse(response_for_places_api.body) # returns a list of places
+        # @attraction_place = @response_place["features"][0]["properties"]["name"] #name of an Attraction
+
+        # @names = @response_place["features"].map { |obj| obj["properties"]["name"] }
     end
 end
