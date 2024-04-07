655da118f25c38dd32684c5560b00d561eee6357
Ron Javellana <Ronapj1991@gmail.com>
2024-04-06 20:22:59 -0400

    add validations

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,6 @@
 class Hotel < ApplicationRecord
     #belongs_to :city
-    
+    validates :hotel_place_id, presence: true
+    validates :name, presence: true
+    validates :address, presence: true
 end
