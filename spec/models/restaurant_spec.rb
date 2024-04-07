cac1672e5015b1accb17e21cb34d4458edb268ae
Ron Javellana <Ronapj1991@gmail.com>
2024-04-06 21:38:26 -0400

    fix test failures

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  subject { Attraction.new(restaurant_place_id: "518709b485b13f5e40598e6ac0d952202d40f00103f901e1520d27000000009203164b656e6e7920526f676572732720526f617374657273", name: "Kenny Rogers' Roasters", address: "Taft Avenue, Manila, 1004 Metro Manila, Philippines") }
+  subject { Restaurant.new(restaurant_place_id: "518709b485b13f5e40598e6ac0d952202d40f00103f901e1520d27000000009203164b656e6e7920526f676572732720526f617374657273", name: "Kenny Rogers' Roasters", address: "Taft Avenue, Manila, 1004 Metro Manila, Philippines") }
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
