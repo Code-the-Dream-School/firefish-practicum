52103c91fececb2c9124c7c97dbc34f4c2c3e500
Ron Javellana <Ronapj1991@gmail.com>
2024-04-06 21:34:32 -0400

    add model testing

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Attraction.new(restaurant_place_id: "518709b485b13f5e40598e6ac0d952202d40f00103f901e1520d27000000009203164b656e6e7920526f676572732720526f617374657273", name: "Kenny Rogers' Roasters", address: "Taft Avenue, Manila, 1004 Metro Manila, Philippines") }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  it { should validate_uniqueness_of(:restaurant_place_id) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:address) }
 end
