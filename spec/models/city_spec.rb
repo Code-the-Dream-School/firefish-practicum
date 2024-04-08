0898014c99d07a11d167ad1cb585b91483ebcd35
muginga72 <lmj.muginga@gmail.com>
2024-04-08 18:43:46 -0400

    Update city test

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -4,7 +4,5 @@ RSpec.describe City, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:city_place_id) }
-    it { should validate_presence_of(:country) }
-    it { should validate_presence_of(:city) }
   end
 end
