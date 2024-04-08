69e8590fee32ea1ce3d503491973f05a141227aa
muginga72 <lmj.muginga@gmail.com>
2024-04-08 18:38:02 -0400

    Fixed the issue with city validation and removed country attribute

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,21 +1,13 @@
 class City < ApplicationRecord
   attribute :name, :string
-  attribute :city, :string
-  attribute :country, :string
   attribute :city_place_id, :string
 
   # Validations
   validates :name, presence: true
   validates :city_place_id, presence: true
-  validates :country, presence: true
-  validates :city, presence: true
 
   # Associations
   has_many :attractions
   has_many :restaurants
   has_many :hotels
-
-  def full_address
-    "#{city}, #{country}" 
-  end
 end
