f32948503cbf1037d80045ded55c9708907f66a8
Ron Javellana <Ronapj1991@gmail.com>
2024-04-08 22:34:12 -0400

    add faker methods to factory for restaurants

--- spec/factories/restaurants.rb
+++ spec/factories/restaurants.rb
@@ -1,15 +1,15 @@
 FactoryBot.define do
   factory :restaurant do
-    restaurant_place_id { "MyString" }
-    name { "MyString" }
-    email { "MyString" }
-    phone { "MyString" }
-    website { "MyString" }
-    address { "MyString" }
-    image_url { "MyString" }
-    cuisine { "MyString" }
-    wheelchair { false }
-    indoor_seating { "MyString" }
-    outdoor_seating { "MyString" }
+    restaurant_place_id { SecureRandom.hex(91) }
+    name { Faker::Address.city }
+    email { Faker::Internet.email }
+    phone { Faker::PhoneNumber.phone_number }
+    website { Faker::Internet.url }
+    address { Faker::Address.full_address }
+    image_url { Faker::LoremFlickr.image }
+    cuisine { Faker::Food.ethnic_category }
+    wheelchair { Faker::Boolean.boolean }
+    indoor_seating { "Yes" }
+    outdoor_seating { "No" }
   end
 end
