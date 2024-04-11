4fae6f7df9d0806293fedf5d3d82fa70f26c775e
Ron Javellana <Ronapj1991@gmail.com>
2024-04-11 00:13:08 -0400

    extract data needed for attractions place type

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,6 @@
 class HomeController < ApplicationController
     def index
-        #sample request
-        # response_for_geocode_api = GeoapifyClient.connection.get("/v1/geocode/search") do |req|
-        #     req.params['text'] = "Venice Italy"
-        #     req.params['format'] = "json"
-        # end #response_for_geocode_api.status => 200
-
-        # response_for_places_api = GeoapifyClient.connection.get("/v2/places") do |req|
-        #     req.params["categories"] = "tourism.attraction"
-        #     req.params["filter"] = "place:517e26b15a4fab284059590e3edef5b74640f00101f901c5ae000000000000c00207"
-        #     req.params["limit"] = 5 #using 5 for now while testing
-        # end #response_for_places_api.status => 200
+        response = SearchLogic.get_place_details("attractions", "5194a2957b81c9024059ece5d2533f6d4840f00101f9016517010000000000c00208")
+        debugger
     end
 end
