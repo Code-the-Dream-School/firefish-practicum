29345650491cd47f9ee921b4b457b92cd630eb3b
Ron Javellana <Ronapj1991@gmail.com>
2024-04-17 21:06:39 -0400

    add custom error

--- lib/place_type_invalid.rb
+++ lib/place_type_invalid.rb
@@ -1,11 +1,2 @@
 class PlaceTypeInvalid < StandardError
-    attr_accessor :message
-
-    def initialize(message)
-        @message = message
-    end
-
-    def to_s
-        "[#{message}] #{super}"
-    end
 end
