822f39d0559e22090bfd9e9e863ac2b92c4d2c7b
Ron Javellana <Ronapj1991@gmail.com>
2024-04-20 22:57:05 -0400

    change from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,30 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_10_212019) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "btree_gin"
+  enable_extension "btree_gist"
+  enable_extension "citext"
+  enable_extension "cube"
+  enable_extension "dblink"
+  enable_extension "dict_int"
+  enable_extension "dict_xsyn"
+  enable_extension "earthdistance"
+  enable_extension "fuzzystrmatch"
+  enable_extension "hstore"
+  enable_extension "intarray"
+  enable_extension "ltree"
+  enable_extension "pg_stat_statements"
+  enable_extension "pg_trgm"
+  enable_extension "pgcrypto"
+  enable_extension "pgrowlocks"
+  enable_extension "pgstattuple"
+  enable_extension "plpgsql"
+  enable_extension "tablefunc"
+  enable_extension "unaccent"
+  enable_extension "uuid-ossp"
+  enable_extension "xml2"
+
   create_table "attractions", force: :cascade do |t|
     t.string "attraction_place_id"
     t.string "name"
@@ -24,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_212019) do
     t.boolean "changing_table"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "city_id", null: false
+    t.bigint "city_id", null: false
     t.index ["city_id"], name: "index_attractions_on_city_id"
   end
 
@@ -48,7 +72,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_212019) do
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "city_id", null: false
+    t.bigint "city_id", null: false
     t.index ["city_id"], name: "index_hotels_on_city_id"
   end
 
@@ -66,7 +90,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_212019) do
     t.string "outdoor_seating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "city_id", null: false
+    t.bigint "city_id", null: false
     t.index ["city_id"], name: "index_restaurants_on_city_id"
   end
 
