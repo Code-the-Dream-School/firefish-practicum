b0e3f31013e3d7c3376f6499eb45236633f91d4a
Ron Javellana <Ronapj1991@gmail.com>
2024-04-22 18:51:30 -0400

    test added code on controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permited_parameters, 
-    if: :devise_controller?
+  # before_action :configure_permited_parameters, 
+  #   if: :devise_controller?
   
-  protected
+  # protected
 
-    def configure_permited_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
-    end
+  #   def configure_permited_parameters
+  #     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
+  #     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+  #   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,24 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
-  def dashboard
-    # display current user
-    @user = current_user
-  end
+  # def dashboard
+  #   # display current user
+  #   @user = current_user
+  # end
 
-  def create
-    @user = User.new(user_params)
-    @user.avatar.attach(io: File.open('path/to/default_profile.png'), filename: 'default_profile.png') unless params[:user][:avatar].present?
-    # Other create logic...
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   @user.avatar.attach(io: File.open('path/to/default_profile.png'), filename: 'default_profile.png') unless params[:user][:avatar].present?
+  #   # Other create logic...
+  # end
 
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      @user.avatar.attach(params[:avatar]) if params[:avatar]
-      redirect_to @user, notice: 'Profile updated successfully.'
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   @user = current_user
+  #   if @user.update(user_params)
+  #     @user.avatar.attach(params[:avatar]) if params[:avatar]
+  #     redirect_to @user, notice: 'Profile updated successfully.'
+  #   else
+  #     render :edit
+  #   end
+  # end
 end
