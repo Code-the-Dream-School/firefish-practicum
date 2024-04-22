b31238e473ac8c5a9d9a5616c7c6619717130735
AigulY <azia.kz.a@gmail.com>
2024-04-22 17:04:24 -0400

    Removed nested firefish-practicum directory from tracking

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,20 @@
 class SearchesController < ApplicationController
-    def index
-        @places = SearchLogic.get_places_from_city_string(params[:city_name], params[:place_type])
-        render params[:place_type]
+  def index
+    if params[:city_name].present?
+      selected_place_types = []
+      selected_place_types << 'attractions' if params[:place_type_attractions] == 'attractions'
+      selected_place_types << 'hotels' if params[:place_type_hotels] == 'hotels'
+      selected_place_types << 'restaurants' if params[:place_type_restaurants] == 'restaurants'
+
+      @places = selected_place_types.flat_map do |place_type|
+        SearchLogic.get_places_from_city_string(params[:city_name], place_type)
+      end
+
+      render :index
+    # else
+    #   flash.now[:alert] = 'Please enter a city name and select a type to search'
+    #   @places = []
+    #   # render :index
     end
+  end
 end
