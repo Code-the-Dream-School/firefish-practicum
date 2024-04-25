7722026229e3455ff24f82ac531d04860190b565
Ron Javellana <Ronapj1991@gmail.com>
2024-04-25 10:32:54 -0400

    test associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
     context "associations" do
         it { should have_and_belong_to_many(:attractions) }
         it { should have_and_belong_to_many(:hotels) }
+        it { should have_and_belong_to_many(:restaurants) }
     end
 end
